818e8a44a07886ebe3dc9233edf9ad7c1071b83b
Vedran Abramovic <vedran.abramovic@ag04.com>
2018-05-26 19:17:16 +0200

    Init commit

---
+++ src/main/java/com/brama/cityrepository/CityRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.brama.cityrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityRepositoryApplication.class, args);
+	}
+}
